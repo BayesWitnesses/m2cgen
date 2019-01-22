e170682ed80191eb7958848cc5bee932e4efc7ac
Viktor Ershov <krinart@gmail.com>
2019-01-22 13:00:46 -0800

    Update code_generator.py

--- m2cgen/ast/interpreters/python/code_generator.py
+++ m2cgen/ast/interpreters/python/code_generator.py
@@ -10,7 +10,7 @@ class PythonCodeGenerator(BaseCodeGenerator):
     tpl_infix_expression = CT("(${left}) ${op} (${right})")
     tpl_return_statement = CT("return ${value}")
     tpl_array_index_access = CT("${array_name}[${index}]")
-    tpl_if_statement = CT("if (${if_def}):")
+    tpl_if_statement = CT("if ${if_def}:")
     tpl_else_statement = CT("else:")
     tpl_var_assignment = CT("${var_name} = ${value}")
 
