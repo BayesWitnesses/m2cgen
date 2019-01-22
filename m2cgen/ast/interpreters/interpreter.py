2ebc9bfb53c8cf3ccbb6a57e38bc1d58ece8241f
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2019-01-22 10:16:25 -0800

    Introduce Random Forest assembler (#13)

--- m2cgen/ast/interpreters/interpreter.py
+++ m2cgen/ast/interpreters/interpreter.py
@@ -1,7 +1,6 @@
 import re
 
 from m2cgen.ast import ast
-from m2cgen.ast.ast import NumExpr, BoolExpr, CtrlExpr
 
 
 class BaseInterpreter:
@@ -12,33 +11,6 @@ class BaseInterpreter:
 
     def interpret(self, expr):
         return self._do_interpret(expr)
-
-    def _do_interpret(self, expr, **kwargs):
-        handler = self._select_handler(expr, (NumExpr, BoolExpr, CtrlExpr))
-        return handler(expr, **kwargs)
-
-    def _select_handler(self, expr, fallback_tpes):
-        handler_name = self._handler_name(type(expr))
-        if hasattr(self, handler_name):
-            return getattr(self, handler_name)
-
-        for expr_tpe in fallback_tpes:
-            if isinstance(expr, expr_tpe):
-                handler_name = self._handler_name(expr_tpe)
-                if hasattr(self, handler_name):
-                    return getattr(self, handler_name)
-
-        raise NotImplementedError(
-            "No handler found for {}".format(type(expr).__name__))
-
-    @staticmethod
-    def _handler_name(expr_tpe):
-        expr_name = BaseInterpreter._normalize_expr_name(expr_tpe.__name__)
-        return "interpret_" + expr_name
-
-    @staticmethod
-    def _normalize_expr_name(name):
-        return re.sub("(?!^)([A-Z]+)", r"_\1", name).lower()
 
     # Default method implementations
 
@@ -84,3 +56,28 @@ class BaseInterpreter:
         return self._cg.array_index_access(
             array_name=self._feature_array_name,
             index=expr.index)
+
+    def _do_interpret(self, expr, **kwargs):
+        try:
+            handler = self._select_handler(expr)
+        except NotImplementedError:
+            if isinstance(expr, ast.TransparentExpr):
+                return self._do_interpret(expr.expr, **kwargs)
+            raise
+        return handler(expr, **kwargs)
+
+    def _select_handler(self, expr):
+        handler_name = self._handler_name(type(expr))
+        if hasattr(self, handler_name):
+            return getattr(self, handler_name)
+        raise NotImplementedError(
+            "No handler found for {}".format(type(expr).__name__))
+
+    @staticmethod
+    def _handler_name(expr_tpe):
+        expr_name = BaseInterpreter._normalize_expr_name(expr_tpe.__name__)
+        return "interpret_" + expr_name
+
+    @staticmethod
+    def _normalize_expr_name(name):
+        return re.sub("(?!^)([A-Z]+)", r"_\1", name).lower()
