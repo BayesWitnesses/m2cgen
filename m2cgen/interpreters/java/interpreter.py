25cf2316decaf325e743b13b4a125880011735d6
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2019-01-22 21:34:16 -0800

    Add subroutine support to the Java Interpreter

--- m2cgen/interpreters/java/interpreter.py
+++ m2cgen/interpreters/java/interpreter.py
@@ -8,11 +8,15 @@ class JavaInterpreter(BaseInterpreter):
                  *args, **kwargs):
         self.package_name = package_name
         self.model_name = model_name
+        self.indent = indent
+        self._subroutine_idx = 0
+        self._subroutine_cgs = []
         cg = JavaCodeGenerator(indent=indent)
         super(JavaInterpreter, self).__init__(cg, *args, **kwargs)
 
     def interpret(self, expr):
         self._cg.reset_state()
+        self._subroutine_idx = 0
 
         if self.package_name:
             self._cg.add_package_name(self.package_name)
@@ -24,7 +28,33 @@ class JavaInterpreter(BaseInterpreter):
                     return_type="double"):
                 last_result = self._do_interpret(expr)
                 self._cg.add_return_statement(last_result)
+            for cg in self._subroutine_cgs:
+                self._cg.add_code_lines(cg.code)
 
         return [
             (self.model_name, self._cg.code),
         ]
+
+    def interpret_subroutine_expr(self, expr, **kwargs):
+        new_cg = JavaCodeGenerator(indent=self.indent)
+        old_cg = self._cg
+
+        method_name = self.get_subroutine_name()
+
+        self._cg = new_cg
+        with self._cg.method_definition(
+                    name=method_name,
+                    args=[("double[]", self._feature_array_name)],
+                    return_type="double"):
+                last_result = self._do_interpret(expr.expr, **kwargs)
+                self._cg.add_return_statement(last_result)
+        self._cg = old_cg
+
+        self._subroutine_cgs.append(new_cg)
+
+        return method_name + "(" + self._feature_array_name + ")"
+
+    def get_subroutine_name(self):
+        subroutine_name = "subroutine" + str(self._subroutine_idx)
+        self._subroutine_idx += 1
+        return subroutine_name
