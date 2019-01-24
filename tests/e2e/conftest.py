193d62d590f725196a831410a1e5975934b20a20
Viktor Ershov <krinart@gmail.com>
2019-01-24 10:11:58 -0800

    Make flake8 happy

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -3,8 +3,8 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--xx", dest='fail_fast_e2e', action="store_const", default=False, const=True,
-        help="Fails e2e test immediately"
+        "--xx", dest='fail_fast_e2e', action="store_const",
+        default=False, const=True, help="Fails e2e test immediately"
     )
 
 
