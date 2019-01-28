9a770b3f44fea9953ce6a9eac64e13a5427e916b
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2019-01-28 07:32:47 -0800

    Refactor e2e tests. Use pytest.mark.parametrize

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from sklearn import linear_model
 from sklearn import tree
@@ -23,63 +24,59 @@ def exec_e2e_test(estimator, executor_cls, model_trainer):
             assert res if isinstance(res, bool) else res.all()
 
 
-def test_java_linear_regression():
-    estimator = linear_model.LinearRegression()
-    exec_e2e_test(estimator, executors.JavaExecutor,
-                  utils.train_model_regression)
-
-
-def test_java_logistic_classification():
-    estimator = linear_model.LogisticRegression()
-    exec_e2e_test(estimator, executors.JavaExecutor,
-                  utils.train_model_classification)
-
-
-def test_java_logistic_classification_binary():
-    estimator = linear_model.LogisticRegression()
-    exec_e2e_test(estimator, executors.JavaExecutor,
-                  utils.train_model_classification_binary)
-
-
-def test_java_tree_regression():
-    estimator = tree.DecisionTreeRegressor(random_state=RANDOM_SEED)
-    exec_e2e_test(estimator, executors.JavaExecutor,
-                  utils.train_model_regression)
-
-
-def test_java_tree_classification():
-    estimator = tree.DecisionTreeClassifier(random_state=RANDOM_SEED)
-    exec_e2e_test(estimator, executors.JavaExecutor,
-                  utils.train_model_classification)
-
-
-def test_java_tree_classification_binary():
-    estimator = tree.DecisionTreeClassifier(random_state=RANDOM_SEED)
-    exec_e2e_test(estimator, executors.JavaExecutor,
-                  utils.train_model_classification_binary)
-
-
-def test_java_ensemble():
-    estimator = ensemble.RandomForestRegressor(n_estimators=10,
-                                               random_state=RANDOM_SEED)
-    exec_e2e_test(estimator, executors.JavaExecutor,
-                  utils.train_model_regression)
-
-
-def test_python_linear():
-    estimator = linear_model.LinearRegression()
-    exec_e2e_test(estimator, executors.PythonExecutor,
-                  utils.train_model_regression)
-
-
-def test_python_tree():
-    estimator = tree.DecisionTreeRegressor(random_state=RANDOM_SEED)
-    exec_e2e_test(estimator, executors.PythonExecutor,
-                  utils.train_model_regression)
-
-
-def test_python_ensemble():
-    estimator = ensemble.RandomForestRegressor(n_estimators=10,
-                                               random_state=RANDOM_SEED)
-    exec_e2e_test(estimator, executors.PythonExecutor,
-                  utils.train_model_regression)
+@pytest.mark.parametrize("estimator,executor_cls,model_trainer", [
+    (
+            linear_model.LinearRegression(),
+            executors.JavaExecutor,
+            utils.train_model_regression
+    ),
+    (
+            linear_model.LogisticRegression(),
+            executors.JavaExecutor,
+            utils.train_model_classification
+    ),
+    (
+            linear_model.LogisticRegression(),
+            executors.JavaExecutor,
+            utils.train_model_classification_binary
+    ),
+    (
+            tree.DecisionTreeRegressor(random_state=RANDOM_SEED),
+            executors.JavaExecutor,
+            utils.train_model_regression
+    ),
+    (
+            tree.DecisionTreeClassifier(random_state=RANDOM_SEED),
+            executors.JavaExecutor,
+            utils.train_model_classification
+    ),
+    (
+            tree.DecisionTreeClassifier(random_state=RANDOM_SEED),
+            executors.JavaExecutor,
+            utils.train_model_classification_binary
+    ),
+    (
+            ensemble.RandomForestRegressor(n_estimators=10,
+                                           random_state=RANDOM_SEED),
+            executors.JavaExecutor,
+            utils.train_model_regression
+    ),
+    (
+            linear_model.LinearRegression(),
+            executors.PythonExecutor,
+            utils.train_model_regression
+    ),
+    (
+            tree.DecisionTreeRegressor(random_state=RANDOM_SEED),
+            executors.PythonExecutor,
+            utils.train_model_regression
+    ),
+    (
+            ensemble.RandomForestRegressor(n_estimators=10,
+                                           random_state=RANDOM_SEED),
+            executors.PythonExecutor,
+            utils.train_model_regression
+    ),
+])
+def test_e2e(estimator, executor_cls, model_trainer):
+    exec_e2e_test(estimator, executor_cls, model_trainer)
