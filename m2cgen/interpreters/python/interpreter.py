808266479c0552a88518dd9ef959d1301165e7d2
Viktor Ershov <krinart@gmail.com>
2019-01-30 09:45:15 -0800

    Rename with_vectors => with_numpy

--- m2cgen/interpreters/python/interpreter.py
+++ m2cgen/interpreters/python/interpreter.py
@@ -4,7 +4,7 @@ from m2cgen.interpreters.python.code_generator import PythonCodeGenerator
 
 class PythonInterpreter(BaseInterpreter):
 
-    with_vectors = False
+    with_numpy = False
 
     def __init__(self, indent=4, *args, **kwargs):
         cg = PythonCodeGenerator(indent=indent)
@@ -19,7 +19,7 @@ class PythonInterpreter(BaseInterpreter):
             last_result = self._do_interpret(expr)
             self._cg.add_return_statement(last_result)
 
-        if self.with_vectors:
+        if self.with_numpy:
             self._cg.add_dependency("numpy", alias="np")
 
         return [
@@ -27,7 +27,7 @@ class PythonInterpreter(BaseInterpreter):
         ]
 
     def interpret_vector_val(self, expr, **kwargs):
-        self.with_vectors = True
+        self.with_numpy = True
         return super().interpret_vector_val(expr, **kwargs)
 
     def interpret_bin_vector_expr(self, expr):
