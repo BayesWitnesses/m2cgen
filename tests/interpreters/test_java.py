7d6965003c75ccd32d415cffde31b7b79e759668
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2019-01-30 14:56:45 -0800

    Fix tests

--- tests/interpreters/test_java.py
+++ tests/interpreters/test_java.py
@@ -229,14 +229,14 @@ public class Model {
     }
     public static double[] addVectors(double[] v1, double[] v2) {
         double[] result = new double[v1.length];
-        for (int i = 0; i < v1.length; i ++) {
+        for (int i = 0; i < v1.length; i++) {
             result[i] = v1[i] + v2[i];
         }
         return result;
     }
     public static double[] mulVectorNumber(double[] v1, double num) {
         double[] result = new double[v1.length];
-        for (int i = 0; i < v1.length; i ++) {
+        for (int i = 0; i < v1.length; i++) {
             result[i] = v1[i] * num;
         }
         return result;
@@ -261,14 +261,14 @@ public class Model {
     }
     public static double[] addVectors(double[] v1, double[] v2) {
         double[] result = new double[v1.length];
-        for (int i = 0; i < v1.length; i ++) {
+        for (int i = 0; i < v1.length; i++) {
             result[i] = v1[i] + v2[i];
         }
         return result;
     }
     public static double[] mulVectorNumber(double[] v1, double num) {
         double[] result = new double[v1.length];
-        for (int i = 0; i < v1.length; i ++) {
+        for (int i = 0; i < v1.length; i++) {
             result[i] = v1[i] * num;
         }
         return result;
