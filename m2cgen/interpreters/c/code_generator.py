e302a85a2a46bff4ade6bf3bc4ce0b43750c10d8
Viktor Ershov <krinart@gmail.com>
2019-01-30 20:00:14 -0800

    Refactor Interpreters/CodeGenerators for C

--- m2cgen/interpreters/c/code_generator.py
+++ m2cgen/interpreters/c/code_generator.py
@@ -1,9 +1,16 @@
 import contextlib
 
 from m2cgen.interpreters.code_generator import CLikeCodeGenerator
+from m2cgen.interpreters.code_generator import CodeTemplate as CT
 
 
 class CCodeGenerator(CLikeCodeGenerator):
+
+    tpl_scalar_var_declare = CT("double ${var_name};")
+    tpl_vector_var_declare = CT("static double *${var_name}[${size}];")
+
+    scalar_type = "double"
+    vector_type = "double *"
 
     def __init__(self, *args, **kwargs):
         super(CCodeGenerator, self).__init__(*args, **kwargs)
@@ -24,3 +31,26 @@ class CCodeGenerator(CLikeCodeGenerator):
         self.add_function_def(name, args, is_vector_output)
         yield
         self.add_block_termination()
+
+    def add_var_declaration(self, expr):
+        # import ipdb; ipdb.set_trace()
+
+        var_name = self.get_var_name()
+
+        if expr.is_vector_output:
+            tpl = self.tpl_vector_var_declare
+            size = expr.size
+        else:
+            tpl = self.tpl_scalar_var_declare
+            size = None
+
+        self.add_code_line(tpl(var_name=var_name, size=size))
+        return var_name
+
+    def _get_var_type(self, is_vector):
+        return (
+            self.vector_type if is_vector
+            else self.scalar_type)
+
+    def _get_input_param_type(self, is_vector):
+        return self._get_var_type(is_vector)
