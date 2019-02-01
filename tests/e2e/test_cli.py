81781d78f723b5e5c9e80f5e63193d4249476fef
Viktor Ershov <krinart@gmail.com>
2019-01-31 20:39:44 -0800

    fix e2e/test_cli

--- tests/e2e/test_cli.py
+++ tests/e2e/test_cli.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pickle
 import subprocess
 
@@ -5,17 +6,18 @@ from sklearn import linear_model
 
 from tests import utils
 
-expected_result = """
-def  score(input):
-    return (((((((((((((36.0068107336504) + ((input[0]) * (-0.10081655845910333))) + ((input[1]) * (0.044035569560795046))) + ((input[2]) * (0.030804434213338117))) + ((input[3]) * (2.9326736094672468))) + ((input[4]) * (-17.09336013214845))) + ((input[5]) * (3.745068220863558))) + ((input[6]) * (0.0033774353698544472))) + ((input[7]) * (-1.4348015681660797))) + ((input[8]) * (0.29015811194282753))) + ((input[9]) * (-0.011463487956327451))) + ((input[10]) * (-0.950001243731317))) + ((input[11]) * (0.010374330909981468))) + ((input[12]) * (-0.5713890449294746))
-""".strip()  # NOQA
-
 
 def execute_test(exec_args):
     result = subprocess.Popen(
         " ".join(exec_args), stdout=subprocess.PIPE, shell=True)
-    actual_result = result.stdout.read().decode("utf-8").strip()
-    assert actual_result == expected_result
+    generated_code = result.stdout.read().decode("utf-8")
+
+    code = generated_code + """
+        result = score([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])"""
+    context = {}
+    exec(code, context)
+
+    assert np.isclose(context["result"], -41.89077994476439)
 
 
 def _prepare_pickled_model(tmp_path):
