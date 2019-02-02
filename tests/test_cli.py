2779ddcb8765bd96c68130f203ce07146e295242
Viktor Ershov <krinart@gmail.com>
2019-02-01 20:32:09 -0800

    Add unit tests for cli.generate_code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,12 @@
+import pickle
+import io
 from unittest import mock
-from io import StringIO
 import sys
 from _pytest import capture
+from sklearn import linear_model
 
 from m2cgen import cli
+from tests import utils
 
 
 def test_file_as_input(tmp_path):
@@ -34,7 +37,7 @@ def test_stdin_as_input(request):
 
 @mock.patch.object(sys, "exit")
 def test_language_is_required(mocked_exit):
-    mocked_stderr = StringIO()
+    mocked_stderr = io.StringIO()
 
     with mock.patch.object(sys, "stderr", new=mocked_stderr):
         cli.parse_args([])
@@ -44,3 +47,21 @@ def test_language_is_required(mocked_exit):
         mocked_stderr.getvalue())
 
     mocked_exit.assert_called_with(2)
+
+
+def test_generate_code():
+    expected_code = """
+def  score(input):
+    return (((((((((((((36.0068107336504) + ((input[0]) * (-0.10081655845910333))) + ((input[1]) * (0.044035569560795046))) + ((input[2]) * (0.030804434213338117))) + ((input[3]) * (2.9326736094672468))) + ((input[4]) * (-17.09336013214845))) + ((input[5]) * (3.745068220863558))) + ((input[6]) * (0.0033774353698544472))) + ((input[7]) * (-1.4348015681660797))) + ((input[8]) * (0.29015811194282753))) + ((input[9]) * (-0.011463487956327451))) + ((input[10]) * (-0.950001243731317))) + ((input[11]) * (0.010374330909981468))) + ((input[12]) * (-0.5713890449294746))
+"""  # NOQA
+
+    estimator = linear_model.LinearRegression()
+    utils.train_model_regression(estimator)
+
+    infile = io.BytesIO()
+
+    pickle.dump(estimator, infile)
+    infile.seek(0)
+
+    actual_code = cli.generate_code(infile, "python")
+    assert actual_code.strip() == expected_code.strip()
