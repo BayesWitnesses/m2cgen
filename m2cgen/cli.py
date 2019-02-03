0700d81764b8a0a820e0a237c016da2ca6b572ba
Viktor Ershov <krinart@gmail.com>
2019-02-03 11:18:15 -0800

    tiny fix 2

--- m2cgen/cli.py
+++ m2cgen/cli.py
@@ -1,12 +1,12 @@
 """CLI for m2cgen.
 
 Example usage:
-    ./m2c <path_to_file> --language java --class_name MyModel \
+    $ m2c <path_to_file> --language java --class_name MyModel \
         --package_name foo.bar.baz
-    ./m2c --language java < <path_to_file>
+    $ m2c --language java < <path_to_file>
 
 Model can also be piped:
-    cat <path_to_file> | m2c --language java
+    # cat <path_to_file> | m2c --language java
 
 """
 import pickle
