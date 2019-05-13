3a911fedd76b822175a9e865bfc0e8299ea2877d
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2019-05-13 13:45:25 -0700

    Add generate code samples for SVM

---
+++ generated_code_examples/go/classification/svm.go
@@ -0,0 +1,84 @@
+import "math"
+func score(input []float64) []float64 {
+    var var0 float64
+    var0 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.0) - (input[0]), 2)) + (math.Pow((2.9) - (input[1]), 2))) + (math.Pow((4.5) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var1 float64
+    var1 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.4) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((4.5) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var2 float64
+    var2 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.1) - (input[0]), 2)) + (math.Pow((2.9) - (input[1]), 2))) + (math.Pow((4.7) - (input[2]), 2))) + (math.Pow((1.4) - (input[3]), 2))))
+    var var3 float64
+    var3 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.6) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((4.5) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var4 float64
+    var4 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.2) - (input[0]), 2)) + (math.Pow((2.2) - (input[1]), 2))) + (math.Pow((4.5) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var5 float64
+    var5 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.0) - (input[0]), 2)) + (math.Pow((2.3) - (input[1]), 2))) + (math.Pow((3.3) - (input[2]), 2))) + (math.Pow((1.0) - (input[3]), 2))))
+    var var6 float64
+    var6 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.5) - (input[0]), 2)) + (math.Pow((2.8) - (input[1]), 2))) + (math.Pow((4.6) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var7 float64
+    var7 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.9) - (input[0]), 2)) + (math.Pow((3.2) - (input[1]), 2))) + (math.Pow((4.8) - (input[2]), 2))) + (math.Pow((1.8) - (input[3]), 2))))
+    var var8 float64
+    var8 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.3) - (input[0]), 2)) + (math.Pow((3.3) - (input[1]), 2))) + (math.Pow((4.7) - (input[2]), 2))) + (math.Pow((1.6) - (input[3]), 2))))
+    var var9 float64
+    var9 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.0) - (input[0]), 2)) + (math.Pow((2.0) - (input[1]), 2))) + (math.Pow((3.5) - (input[2]), 2))) + (math.Pow((1.0) - (input[3]), 2))))
+    var var10 float64
+    var10 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.7) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((5.0) - (input[2]), 2))) + (math.Pow((1.7) - (input[3]), 2))))
+    var var11 float64
+    var11 = math.Exp(((0) - (0.25)) * ((((math.Pow((7.0) - (input[0]), 2)) + (math.Pow((3.2) - (input[1]), 2))) + (math.Pow((4.7) - (input[2]), 2))) + (math.Pow((1.4) - (input[3]), 2))))
+    var var12 float64
+    var12 = math.Exp(((0) - (0.25)) * ((((math.Pow((4.9) - (input[0]), 2)) + (math.Pow((2.4) - (input[1]), 2))) + (math.Pow((3.3) - (input[2]), 2))) + (math.Pow((1.0) - (input[3]), 2))))
+    var var13 float64
+    var13 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.3) - (input[0]), 2)) + (math.Pow((2.5) - (input[1]), 2))) + (math.Pow((4.9) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var14 float64
+    var14 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.0) - (input[0]), 2)) + (math.Pow((2.7) - (input[1]), 2))) + (math.Pow((5.1) - (input[2]), 2))) + (math.Pow((1.6) - (input[3]), 2))))
+    var var15 float64
+    var15 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.7) - (input[0]), 2)) + (math.Pow((3.1) - (input[1]), 2))) + (math.Pow((4.7) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var16 float64
+    var16 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.0) - (input[0]), 2)) + (math.Pow((3.4) - (input[1]), 2))) + (math.Pow((4.5) - (input[2]), 2))) + (math.Pow((1.6) - (input[3]), 2))))
+    var var17 float64
+    var17 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.8) - (input[0]), 2)) + (math.Pow((2.8) - (input[1]), 2))) + (math.Pow((4.8) - (input[2]), 2))) + (math.Pow((1.4) - (input[3]), 2))))
+    var var18 float64
+    var18 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.1) - (input[0]), 2)) + (math.Pow((3.8) - (input[1]), 2))) + (math.Pow((1.9) - (input[2]), 2))) + (math.Pow((0.4) - (input[3]), 2))))
+    var var19 float64
+    var19 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.7) - (input[0]), 2)) + (math.Pow((4.4) - (input[1]), 2))) + (math.Pow((1.5) - (input[2]), 2))) + (math.Pow((0.4) - (input[3]), 2))))
+    var var20 float64
+    var20 = math.Exp(((0) - (0.25)) * ((((math.Pow((4.3) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((1.1) - (input[2]), 2))) + (math.Pow((0.1) - (input[3]), 2))))
+    var var21 float64
+    var21 = math.Exp(((0) - (0.25)) * ((((math.Pow((4.5) - (input[0]), 2)) + (math.Pow((2.3) - (input[1]), 2))) + (math.Pow((1.3) - (input[2]), 2))) + (math.Pow((0.3) - (input[3]), 2))))
+    var var22 float64
+    var22 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.8) - (input[0]), 2)) + (math.Pow((2.7) - (input[1]), 2))) + (math.Pow((5.1) - (input[2]), 2))) + (math.Pow((1.9) - (input[3]), 2))))
+    var var23 float64
+    var23 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.3) - (input[0]), 2)) + (math.Pow((2.7) - (input[1]), 2))) + (math.Pow((4.9) - (input[2]), 2))) + (math.Pow((1.8) - (input[3]), 2))))
+    var var24 float64
+    var24 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.0) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((4.8) - (input[2]), 2))) + (math.Pow((1.8) - (input[3]), 2))))
+    var var25 float64
+    var25 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.3) - (input[0]), 2)) + (math.Pow((2.8) - (input[1]), 2))) + (math.Pow((5.1) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var26 float64
+    var26 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.1) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((4.9) - (input[2]), 2))) + (math.Pow((1.8) - (input[3]), 2))))
+    var var27 float64
+    var27 = math.Exp(((0) - (0.25)) * ((((math.Pow((7.7) - (input[0]), 2)) + (math.Pow((2.6) - (input[1]), 2))) + (math.Pow((6.9) - (input[2]), 2))) + (math.Pow((2.3) - (input[3]), 2))))
+    var var28 float64
+    var28 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.9) - (input[0]), 2)) + (math.Pow((3.1) - (input[1]), 2))) + (math.Pow((5.1) - (input[2]), 2))) + (math.Pow((2.3) - (input[3]), 2))))
+    var var29 float64
+    var29 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.3) - (input[0]), 2)) + (math.Pow((2.5) - (input[1]), 2))) + (math.Pow((5.0) - (input[2]), 2))) + (math.Pow((1.9) - (input[3]), 2))))
+    var var30 float64
+    var30 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.1) - (input[0]), 2)) + (math.Pow((2.6) - (input[1]), 2))) + (math.Pow((5.6) - (input[2]), 2))) + (math.Pow((1.4) - (input[3]), 2))))
+    var var31 float64
+    var31 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.5) - (input[0]), 2)) + (math.Pow((3.2) - (input[1]), 2))) + (math.Pow((5.1) - (input[2]), 2))) + (math.Pow((2.0) - (input[3]), 2))))
+    var var32 float64
+    var32 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.3) - (input[0]), 2)) + (math.Pow((3.3) - (input[1]), 2))) + (math.Pow((6.0) - (input[2]), 2))) + (math.Pow((2.5) - (input[3]), 2))))
+    var var33 float64
+    var33 = math.Exp(((0) - (0.25)) * ((((math.Pow((4.9) - (input[0]), 2)) + (math.Pow((2.5) - (input[1]), 2))) + (math.Pow((4.5) - (input[2]), 2))) + (math.Pow((1.7) - (input[3]), 2))))
+    var var34 float64
+    var34 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.9) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((5.1) - (input[2]), 2))) + (math.Pow((1.8) - (input[3]), 2))))
+    var var35 float64
+    var35 = math.Exp(((0) - (0.25)) * ((((math.Pow((5.6) - (input[0]), 2)) + (math.Pow((2.8) - (input[1]), 2))) + (math.Pow((4.9) - (input[2]), 2))) + (math.Pow((2.0) - (input[3]), 2))))
+    var var36 float64
+    var36 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.0) - (input[0]), 2)) + (math.Pow((2.2) - (input[1]), 2))) + (math.Pow((5.0) - (input[2]), 2))) + (math.Pow((1.5) - (input[3]), 2))))
+    var var37 float64
+    var37 = math.Exp(((0) - (0.25)) * ((((math.Pow((6.5) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((5.2) - (input[2]), 2))) + (math.Pow((2.0) - (input[3]), 2))))
+    var var38 float64
+    var38 = math.Exp(((0) - (0.25)) * ((((math.Pow((7.9) - (input[0]), 2)) + (math.Pow((3.8) - (input[1]), 2))) + (math.Pow((6.4) - (input[2]), 2))) + (math.Pow((2.0) - (input[3]), 2))))
+    var var39 float64
+    var39 = math.Exp(((0) - (0.25)) * ((((math.Pow((7.2) - (input[0]), 2)) + (math.Pow((3.0) - (input[1]), 2))) + (math.Pow((5.8) - (input[2]), 2))) + (math.Pow((1.6) - (input[3]), 2))))
+    return []float64{((((((((((((((((((((((-0.058273877784106996) + ((var0) * (-0.0))) + ((var1) * (-0.0))) + ((var2) * (-0.0))) + ((var3) * (-0.0))) + ((var4) * (-0.0))) + ((var5) * (-0.39827104823830767))) + ((var6) * (-0.0))) + ((var7) * (-0.0))) + ((var8) * (-0.0))) + ((var9) * (-0.0))) + ((var10) * (-0.1945483141137432))) + ((var11) * (-0.5731507539020576))) + ((var12) * (-1.0))) + ((var13) * (-0.0))) + ((var14) * (-0.0))) + ((var15) * (-0.0))) + ((var16) * (-0.0))) + ((var17) * (-0.0))) + ((var18) * (0.7232997964245547))) + ((var19) * (0.4426703198295538))) + ((var20) * (0.0))) + ((var21) * (1.0)), ((((((((((((((((((((((-0.1675575761469295) + ((var22) * (-0.0))) + ((var23) * (-0.0))) + ((var24) * (-0.0))) + ((var25) * (-0.0))) + ((var26) * (-0.0))) + ((var27) * (-0.42842234431180903))) + ((var28) * (-0.16142399554136144))) + ((var29) * (-0.0))) + ((var30) * (-0.0))) + ((var31) * (-0.0))) + ((var32) * (-0.11716468028552147))) + ((var33) * (-0.732591094590368))) + ((var34) * (-0.0))) + ((var35) * (-0.0))) + ((var36) * (-0.10069216545035711))) + ((var37) * (-0.0))) + ((var38) * (-0.4050989417293007))) + ((var39) * (-0.0))) + ((var18) * (0.0))) + ((var19) * (0.9603758754297508))) + ((var20) * (0.017307626578303974))) + ((var21) * (0.9677097199006631)), ((((((((((((((((((((((((((((((((((((-0.1690193167441151) + ((var22) * (-1.0))) + ((var23) * (-1.0))) + ((var24) * (-1.0))) + ((var25) * (-1.0))) + ((var26) * (-1.0))) + ((var27) * (-0.0))) + ((var28) * (-0.6997374221624343))) + ((var29) * (-1.0))) + ((var30) * (-0.5563222234148923))) + ((var31) * (-1.0))) + ((var32) * (-0.0))) + ((var33) * (-1.0))) + ((var34) * (-1.0))) + ((var35) * (-1.0))) + ((var36) * (-1.0))) + ((var37) * (-1.0))) + ((var38) * (-0.18584105901562897))) + ((var39) * (-0.4083559420777223))) + ((var0) * (1.0))) + ((var1) * (1.0))) + ((var2) * (1.0))) + ((var3) * (1.0))) + ((var4) * (1.0))) + ((var5) * (0.0))) + ((var6) * (1.0))) + ((var7) * (1.0))) + ((var8) * (1.0))) + ((var9) * (0.3840890619619074))) + ((var10) * (1.0))) + ((var11) * (0.021588961408824672))) + ((var12) * (0.0))) + ((var13) * (1.0))) + ((var14) * (1.0))) + ((var15) * (1.0))) + ((var16) * (0.4445786232999459))) + ((var17) * (1.0))}
+}
