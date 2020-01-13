b1b60e5242766922180cb8e0a443668d2e16c349
Nikita Titov <nekit94-12@hotmail.com>
2020-01-14 01:18:33 +0300

    Fix Travis failure reporting (#142)

---
+++ .travis/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+cd $TRAVIS_BUILD_DIR
+
+if [[ $TEST == "API" ]]; then
+  flake8 .
+  pytest -v tests/ --cov=m2cgen/ --ignore=tests/e2e/
+  coveralls
+fi
+
+if [[ $TEST == "E2E" ]]; then
+  python setup.py install
+  rm -rfd m2cgen/
+  pytest -v "-m=$LANG" tests/e2e/
+fi
