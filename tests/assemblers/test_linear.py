9812adc6c9510f6c9ac755c579d2dcb256bf0d5b
StrikerRUS <nekit94-12@hotmail.com>
2020-01-29 05:41:25 +0300

    added API tests

--- tests/assemblers/test_linear.py
+++ tests/assemblers/test_linear.py
@@ -1,4 +1,6 @@
 import numpy as np
+import statsmodels.api as sm
+import pytest
 from sklearn import linear_model
 
 from m2cgen import assemblers, ast
@@ -127,3 +129,156 @@ def test_binary_class():
         ast.BinNumOpType.ADD)
 
     assert utils.cmp_exprs(actual, expected)
+
+
+def test_statsmodels_wo_const():
+    estimator = utils.StatsmodelsSklearnLikeWrapper(sm.GLS, {})
+    _, __, estimator = utils.get_regression_model_trainer()(estimator)
+
+    assembler = assemblers.StatsmodelsLinearModelAssembler(estimator)
+    actual = assembler.assemble()
+
+    feature_weight_mul = [
+        ast.BinNumExpr(
+            ast.FeatureRef(0),
+            ast.NumVal(-0.0926871267),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(1),
+            ast.NumVal(0.0482139967),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(2),
+            ast.NumVal(-0.0075524567),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(3),
+            ast.NumVal(2.9965313383),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(4),
+            ast.NumVal(-3.0877925575),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(5),
+            ast.NumVal(5.9546630146),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(6),
+            ast.NumVal(-0.0073548271),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(7),
+            ast.NumVal(-0.9828206079),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(8),
+            ast.NumVal(0.1727389546),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(9),
+            ast.NumVal(-0.0094218658),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(10),
+            ast.NumVal(-0.3931071261),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(11),
+            ast.NumVal(0.0149656744),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(12),
+            ast.NumVal(-0.4133835832),
+            ast.BinNumOpType.MUL),
+    ]
+
+    expected = assemblers.utils.apply_op_to_expressions(
+        ast.BinNumOpType.ADD,
+        ast.NumVal(0.0),
+        *feature_weight_mul)
+
+    assert utils.cmp_exprs(actual, expected)
+
+
+def test_statsmodels_w_const():
+    estimator = utils.StatsmodelsSklearnLikeWrapper(
+        sm.GLS,
+        dict(init=dict(fit_intercept=True)))
+    _, __, estimator = utils.get_regression_model_trainer()(estimator)
+
+    assembler = assemblers.StatsmodelsLinearModelAssembler(estimator)
+    actual = assembler.assemble()
+
+    feature_weight_mul = [
+        ast.BinNumExpr(
+            ast.FeatureRef(0),
+            ast.NumVal(-0.1085910250),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(1),
+            ast.NumVal(0.0441988987),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(2),
+            ast.NumVal(0.0174669054),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(3),
+            ast.NumVal(2.8323210870),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(4),
+            ast.NumVal(-18.4837486980),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(5),
+            ast.NumVal(3.8354955484),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(6),
+            ast.NumVal(0.0001409165),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(7),
+            ast.NumVal(-1.5040340047),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(8),
+            ast.NumVal(0.3106174852),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(9),
+            ast.NumVal(-0.0123066500),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(10),
+            ast.NumVal(-0.9736183985),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(11),
+            ast.NumVal(0.0094039648),
+            ast.BinNumOpType.MUL),
+        ast.BinNumExpr(
+            ast.FeatureRef(12),
+            ast.NumVal(-0.5203427347),
+            ast.BinNumOpType.MUL),
+    ]
+
+    expected = assemblers.utils.apply_op_to_expressions(
+        ast.BinNumOpType.ADD,
+        ast.NumVal(37.1353468527),
+        *feature_weight_mul)
+
+    assert utils.cmp_exprs(actual, expected)
+
+
+@pytest.mark.xfail(raises=ValueError, strict=True)
+def test_statsmodels_unknown_constant_position():
+    estimator = utils.StatsmodelsSklearnLikeWrapper(
+        sm.GLS,
+        dict(init=dict(hasconst=True)))
+    _, __, estimator = utils.get_regression_model_trainer()(estimator)
+
+    assembler = assemblers.StatsmodelsLinearModelAssembler(estimator)
+    assembler.assemble()
