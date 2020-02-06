81c3e6a78410da66b6a4dfa914322454a99f1c12
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2020-02-06 08:12:08 -0800

    In Java interpreter ignore subroutines and perform code split based on the AST size (#158)

---
+++ tests/test_ast.py
@@ -0,0 +1,70 @@
+from m2cgen import ast
+
+
+def test_count_exprs():
+    assert ast.count_exprs(
+        ast.BinNumExpr(ast.NumVal(1), ast.NumVal(2), ast.BinNumOpType.ADD)
+    ) == 3
+
+    assert ast.count_exprs(
+        ast.ExpExpr(ast.NumVal(2))
+    ) == 2
+
+    assert ast.count_exprs(
+        ast.VectorVal([
+            ast.NumVal(2),
+            ast.TanhExpr(ast.NumVal(3))
+        ])
+    ) == 4
+
+    assert ast.count_exprs(
+        ast.IfExpr(
+            ast.CompExpr(ast.NumVal(2), ast.NumVal(0), ast.CompOpType.GT),
+            ast.NumVal(3),
+            ast.NumVal(4),
+        )
+    ) == 6
+
+    assert ast.count_exprs(ast.SubroutineExpr(ast.NumVal(1))) == 1
+
+
+def test_count_exprs_exclude_list():
+    assert ast.count_exprs(
+        ast.BinNumExpr(ast.NumVal(1), ast.NumVal(2), ast.BinNumOpType.ADD),
+        exclude_list={ast.BinExpr, ast.NumVal}
+    ) == 0
+
+    assert ast.count_exprs(
+        ast.BinNumExpr(ast.NumVal(1), ast.NumVal(2), ast.BinNumOpType.ADD),
+        exclude_list={ast.BinNumExpr}
+    ) == 2
+
+
+def test_count_all_exprs_types():
+    expr = ast.BinVectorNumExpr(
+        ast.BinVectorExpr(
+            ast.VectorVal([
+                ast.ExpExpr(ast.NumVal(2)),
+                ast.PowExpr(ast.NumVal(2), ast.NumVal(3)),
+                ast.TanhExpr(ast.NumVal(1)),
+                ast.BinNumExpr(
+                    ast.NumVal(0),
+                    ast.FeatureRef(0),
+                    ast.BinNumOpType.ADD)
+            ]),
+            ast.VectorVal([
+                ast.NumVal(1),
+                ast.NumVal(2),
+                ast.NumVal(3),
+                ast.FeatureRef(1)
+            ]),
+            ast.BinNumOpType.SUB),
+        ast.SubroutineExpr(
+            ast.IfExpr(
+                ast.CompExpr(ast.NumVal(2), ast.NumVal(0), ast.CompOpType.GT),
+                ast.NumVal(3),
+                ast.NumVal(4),
+            )),
+        ast.BinNumOpType.MUL)
+
+    assert ast.count_exprs(expr) == 24
