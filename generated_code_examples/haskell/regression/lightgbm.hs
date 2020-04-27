70f6bef7947c94d54d1f993fdb6a0f05b705c1e8
Nikita Titov <nekit94-08@mail.ru>
2020-04-27 21:18:04 +0300

    Generate Haskell examples (#198)

---
+++ generated_code_examples/haskell/regression/lightgbm.hs
@@ -0,0 +1,33 @@
+module Model where
+score :: [Double] -> Double
+score input =
+    ((0) + (func0)) + (func1)
+    where
+        func0 =
+            if (((input) !! (5)) > (6.918000000000001))
+                then
+                    if (((input) !! (5)) > (7.437))
+                        then
+                            24.81112131071211
+                        else
+                            23.5010290754961
+                else
+                    if (((input) !! (12)) > (14.365))
+                        then
+                            21.796569516771488
+                        else
+                            22.640634908349323
+        func1 =
+            if (((input) !! (12)) > (9.63))
+                then
+                    if (((input) !! (12)) > (19.23))
+                        then
+                            -0.9218520876020193
+                        else
+                            -0.30490175606373926
+                else
+                    if (((input) !! (5)) > (7.437))
+                        then
+                            2.028554553190867
+                        else
+                            0.45970642160364367
---
+++ generated_code_examples/haskell/regression/random_forest.hs
@@ -0,0 +1,41 @@
+module Model where
+score :: [Double] -> Double
+score input =
+    ((func0) + (func1)) * (0.5)
+    where
+        func0 =
+            if (((input) !! (12)) <= (8.91))
+                then
+                    if (((input) !! (5)) <= (6.902))
+                        then
+                            if (((input) !! (7)) <= (1.48495))
+                                then
+                                    50.0
+                                else
+                                    25.320000000000004
+                        else
+                            38.34810126582279
+                else
+                    if (((input) !! (0)) <= (5.84803))
+                        then
+                            19.99185520361991
+                        else
+                            12.102469135802467
+        func1 =
+            if (((input) !! (12)) <= (9.725))
+                then
+                    if (((input) !! (5)) <= (7.4525))
+                        then
+                            if (((input) !! (5)) <= (6.7539997))
+                                then
+                                    24.801739130434775
+                                else
+                                    32.47230769230769
+                        else
+                            47.075
+                else
+                    if (((input) !! (12)) <= (15.0))
+                        then
+                            20.44094488188976
+                        else
+                            14.823214285714291
