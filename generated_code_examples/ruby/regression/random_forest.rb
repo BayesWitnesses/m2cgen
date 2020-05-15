b6f4d541163c21188894a712261621c7d0421b9d
Nikita Titov <nekit94-08@mail.ru>
2020-05-15 23:03:02 +0300

    Generate Ruby code examples (#215)

---
+++ generated_code_examples/ruby/regression/random_forest.rb
@@ -0,0 +1,37 @@
+def score(input)
+    if (input[12]) <= (8.91)
+        if (input[5]) <= (6.902)
+            if (input[7]) <= (1.48495)
+                var0 = 50.0
+            else
+                var0 = 25.320000000000004
+            end
+        else
+            var0 = 38.34810126582279
+        end
+    else
+        if (input[0]) <= (5.84803)
+            var0 = 19.99185520361991
+        else
+            var0 = 12.102469135802467
+        end
+    end
+    if (input[12]) <= (9.725)
+        if (input[5]) <= (7.4525)
+            if (input[5]) <= (6.7539997)
+                var1 = 24.801739130434775
+            else
+                var1 = 32.47230769230769
+            end
+        else
+            var1 = 47.075
+        end
+    else
+        if (input[12]) <= (15.0)
+            var1 = 20.44094488188976
+        else
+            var1 = 14.823214285714291
+        end
+    end
+    ((var0) + (var1)) * (0.5)
+end
---
+++ generated_code_examples/ruby/regression/xgboost.rb
@@ -0,0 +1,29 @@
+def score(input)
+    if (input[12]) >= (9.725)
+        if (input[12]) >= (16.085)
+            var0 = 4.09124994
+        else
+            var0 = 5.89006901
+        end
+    else
+        if (input[5]) >= (6.941)
+            var0 = 11.241127
+        else
+            var0 = 7.45130444
+        end
+    end
+    if (input[12]) >= (5.1549997)
+        if (input[12]) >= (14.4)
+            var1 = 3.00165963
+        else
+            var1 = 4.8301239
+        end
+    else
+        if (input[5]) >= (7.406)
+            var1 = 9.98775578
+        else
+            var1 = 6.78264093
+        end
+    end
+    ((0.5) + (var0)) + (var1)
+end
