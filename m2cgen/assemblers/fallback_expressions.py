ca8bf0ed68bceaefeacafee82e45c254efbe75e5
Nikita Titov <nekit94-08@mail.ru>
2020-05-22 20:05:32 +0300

    Add fallback expressions for Sqrt and Exp functions (#220)

--- m2cgen/assemblers/fallback_expressions.py
+++ m2cgen/assemblers/fallback_expressions.py
@@ -30,6 +30,20 @@ def tanh(expr):
             tanh_expr))
 
 
+def sqrt(expr, to_reuse=False):
+    return ast.PowExpr(
+        base_expr=expr,
+        exp_expr=ast.NumVal(0.5),
+        to_reuse=to_reuse)
+
+
+def exp(expr, to_reuse=False):
+    return ast.PowExpr(
+        base_expr=ast.NumVal(2.71828182845904523536028747135),
+        exp_expr=expr,
+        to_reuse=to_reuse)
+
+
 def sigmoid(expr, to_reuse=False):
     neg_expr = ast.BinNumExpr(ast.NumVal(0), expr, ast.BinNumOpType.SUB)
     exp_expr = ast.ExpExpr(neg_expr)
