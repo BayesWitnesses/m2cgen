81152436032af417fe267b6838e60adeaa159f34
Nikita Titov <nekit94-08@mail.ru>
2020-06-23 19:13:44 +0300

    Add tests for expression equality and hash (#249)

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,4 +1,6 @@
+from copy import deepcopy
 import numpy as np
+
 from m2cgen import ast
 
 
@@ -41,42 +43,53 @@ def test_count_exprs_exclude_list():
     ) == 2
 
 
-def test_count_all_exprs_types():
-    expr = ast.BinVectorNumExpr(
-        ast.BinVectorExpr(
+EXPR_WITH_ALL_EXPRS = ast.BinVectorNumExpr(
+    ast.BinVectorExpr(
+        ast.VectorVal([
+            ast.AbsExpr(ast.NumVal(-2)),
+            ast.ExpExpr(ast.NumVal(2)),
+            ast.LogExpr(ast.NumVal(2)),
+            ast.Log1pExpr(ast.NumVal(2)),
+            ast.SqrtExpr(ast.NumVal(2)),
+            ast.PowExpr(ast.NumVal(2), ast.NumVal(3)),
+            ast.TanhExpr(ast.NumVal(1)),
+            ast.BinNumExpr(
+                ast.NumVal(0),
+                ast.FeatureRef(0),
+                ast.BinNumOpType.ADD)
+        ]),
+        ast.IdExpr(
             ast.VectorVal([
-                ast.AbsExpr(ast.NumVal(-2)),
-                ast.ExpExpr(ast.NumVal(2)),
-                ast.LogExpr(ast.NumVal(2)),
-                ast.Log1pExpr(ast.NumVal(2)),
-                ast.SqrtExpr(ast.NumVal(2)),
-                ast.PowExpr(ast.NumVal(2), ast.NumVal(3)),
-                ast.TanhExpr(ast.NumVal(1)),
-                ast.BinNumExpr(
-                    ast.NumVal(0),
-                    ast.FeatureRef(0),
-                    ast.BinNumOpType.ADD)
-            ]),
-            ast.IdExpr(
-                ast.VectorVal([
-                    ast.NumVal(1),
-                    ast.NumVal(2),
-                    ast.NumVal(3),
-                    ast.NumVal(4),
-                    ast.NumVal(5),
-                    ast.NumVal(6),
-                    ast.NumVal(7),
-                    ast.FeatureRef(1)
-                ])),
-            ast.BinNumOpType.SUB),
-        ast.IfExpr(
-            ast.CompExpr(ast.NumVal(2), ast.NumVal(0), ast.CompOpType.GT),
-            ast.NumVal(3),
-            ast.NumVal(4),
-        ),
-        ast.BinNumOpType.MUL)
+                ast.NumVal(1),
+                ast.NumVal(2),
+                ast.NumVal(3),
+                ast.NumVal(4),
+                ast.NumVal(5),
+                ast.NumVal(6),
+                ast.NumVal(7),
+                ast.FeatureRef(1)
+            ])),
+        ast.BinNumOpType.SUB),
+    ast.IfExpr(
+        ast.CompExpr(ast.NumVal(2), ast.NumVal(0), ast.CompOpType.GT),
+        ast.NumVal(3),
+        ast.NumVal(4),
+    ),
+    ast.BinNumOpType.MUL)
 
-    assert ast.count_exprs(expr) == 37
+
+def test_count_all_exprs_types():
+    assert ast.count_exprs(EXPR_WITH_ALL_EXPRS) == 37
+
+
+def test_exprs_equality():
+    expr_copy = deepcopy(EXPR_WITH_ALL_EXPRS)
+    assert EXPR_WITH_ALL_EXPRS == expr_copy
+
+
+def test_exprs_hash():
+    expr_copy = deepcopy(EXPR_WITH_ALL_EXPRS)
+    assert hash(EXPR_WITH_ALL_EXPRS) == hash(expr_copy)
 
 
 def test_num_val():
