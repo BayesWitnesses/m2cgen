48e811a3b82005218116aa721dcb047ad33e7915
StrikerRUS <nekit94-12@hotmail.com>
2020-07-24 02:39:03 +0300

    generate F# code examples

---
+++ generated_code_examples/f_sharp/regression/decision_tree.fs
@@ -0,0 +1,16 @@
+let score (input : double list) =
+    let func0 =
+        if ((input.[5]) <= (6.941)) then
+            if ((input.[12]) <= (14.4)) then
+                if ((input.[7]) <= (1.38485)) then
+                    45.58
+                else
+                    22.939004149377574
+            else
+                14.910404624277467
+        else
+            if ((input.[5]) <= (7.4370003)) then
+                32.11304347826088
+            else
+                45.096666666666664
+    func0
