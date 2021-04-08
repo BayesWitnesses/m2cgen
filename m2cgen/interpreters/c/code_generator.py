2ec3c6b033a3f2d684e58f8bdf656cf3c8ac686f
Nikita Titov <nekit94-08@mail.ru>
2021-04-09 02:39:02 +0300

    Update code_generator.py

--- m2cgen/interpreters/c/code_generator.py
+++ m2cgen/interpreters/c/code_generator.py
@@ -5,8 +5,8 @@ from m2cgen.interpreters.code_generator import CLikeCodeGenerator, CodeTemplate
 
 class CCodeGenerator(CLikeCodeGenerator):
 
-    tpl_scalar_var_declare = CT("double {var_name};")
-    tpl_vector_var_declare = CT("double {var_name}[{size}];")
+    tpl_scalar_var_declare = CodeTemplate("double {var_name};")
+    tpl_vector_var_declare = CodeTemplate("double {var_name}[{size}];")
 
     scalar_type = "double"
     vector_type = "double *"
