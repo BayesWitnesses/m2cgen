740e02bd704b6cd9da2e058498144d72092e8333
StrikerRUS <nekit94-12@hotmail.com>
2021-05-03 02:36:26 +0300

    fix Dart

--- m2cgen/interpreters/dart/softmax.dart
+++ m2cgen/interpreters/dart/softmax.dart
@@ -1,6 +1,6 @@
 List<double> softmax(List<double> x) {
     int size = x.length;
-    List<double> result = new List<double>(size);
+    List<double> result = new List<double>.filled(size, 0.0);
     double maxElem = x.reduce(max);
     double sum = 0.0;
     for (int i = 0; i < size; ++i) {
