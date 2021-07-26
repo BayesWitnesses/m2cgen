52fa6b0d0ddf99f85aadd6e8045b21e7c0323570
Nikita Titov <nekit94-08@mail.ru>
2021-07-26 05:38:57 +0300

    Better avoid circular imports (#427)

--- m2cgen/assemblers/ensemble.py
+++ m2cgen/assemblers/ensemble.py
@@ -1,23 +1,18 @@
-from m2cgen import ast
-from m2cgen.assemblers.base import ModelAssembler
+from m2cgen import assemblers, ast
 
 
-class RandomForestModelAssembler(ModelAssembler):
+class RandomForestModelAssembler(assemblers.base.ModelAssembler):
 
     def assemble(self):
-        # import here to avoid circular import error
-        from m2cgen.assemblers import TreeModelAssembler, utils
-
         trees = self.model.estimators_
 
         def assemble_tree_expr(t):
-            assembler = TreeModelAssembler(t)
+            assembler = assemblers.TreeModelAssembler(t)
 
             return assembler.assemble()
 
         assembled_trees = [assemble_tree_expr(t) for t in trees]
-        return utils.apply_bin_op(
-            utils.apply_op_to_expressions(ast.BinNumOpType.ADD,
-                                          *assembled_trees),
+        return assemblers.utils.apply_bin_op(
+            assemblers.utils.apply_op_to_expressions(ast.BinNumOpType.ADD, *assembled_trees),
             ast.NumVal(1 / self.model.n_estimators),
             ast.BinNumOpType.MUL)
