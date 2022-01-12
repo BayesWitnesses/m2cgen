8351317678e5b0956a45a3b2e9357613f5624d14
Nikita Titov <nekit94-08@mail.ru>
2022-01-13 00:44:34 +0300

    Update test.sh

--- .ci/test.sh
+++ .ci/test.sh
@@ -8,7 +8,7 @@ if [[ $TEST == "API" ]]; then
   pytest -v tests/ --cov=m2cgen/ --cov-report=xml:coverage.xml --ignore=tests/e2e/
   curl -Os https://uploader.codecov.io/latest/linux/codecov
   chmod +x codecov
-  ./codecov -f coverage.xml -Z
+  ./codecov -t c3ce359c-f789-41e2-9059-2643131179b5 -f coverage.xml -Z
 fi
 
 if [[ $TEST == "E2E" ]]; then
