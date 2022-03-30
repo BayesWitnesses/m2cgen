0ce024584c9e8daf956391b021bca9ef9fe4661b
Nikita Titov <nekit94-12@hotmail.com>
2022-03-31 00:11:56 +0300

    Update test.sh

--- .ci/test.sh
+++ .ci/test.sh
@@ -3,12 +3,17 @@
 set -e
 
 if [[ $TEST == "API" ]]; then
+  # Run code linters
   flake8 .
   isort . --check-only
+  # Run tests
   pytest -v tests/ --cov=m2cgen/ --cov-report=xml:coverage.xml --ignore=tests/e2e/
+  # Run code coverage
   wget -q https://uploader.codecov.io/latest/linux/codecov -O codecov
   chmod +x codecov
   ./codecov -f coverage.xml -Z
+  # Generate code examples
+  python setup.py develop
   python tools/generate_code_examples.py ./generated_code_examples
 fi
 
