e6e16ebce949ffaaa1df36f5e76dc6ab19f0e2e3
Nikita Titov <nekit94-12@hotmail.com>
2022-04-09 03:45:03 +0300

    Generate artifact with code examples (#512)
    * Update test.sh
    
    * Update main.yml
    
    * Update test.sh
    
    * Update main.yml
    
    * Update main.yml
    
    * Update test.sh

--- .ci/test.sh
+++ .ci/test.sh
@@ -3,12 +3,18 @@
 set -e
 
 if [[ $TEST == "API" ]]; then
+  # Run code linters
   flake8 .
   isort . --check-only
+  # Run tests with code coverage
   pytest -v tests/ --cov=m2cgen/ --cov-report=xml:coverage.xml --ignore=tests/e2e/
+  # Upload code coverage
   wget -q https://uploader.codecov.io/latest/linux/codecov -O codecov
   chmod +x codecov
   ./codecov -f coverage.xml -Z
+  # Generate code examples
+  python setup.py develop
+  python tools/generate_code_examples.py ./generated_code_examples
 fi
 
 if [[ $TEST == "E2E" ]]; then
